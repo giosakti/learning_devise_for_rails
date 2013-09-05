99145bfbaa0622620b3a21e17b10fdb6ac67fc73
Giovanni Sakti <giosakti@starqle.com>
2013-09-05 14:11:57 +0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningDeviseForRails::Application.config.secret_key_base = 'f55dd171ce113a0eb3d0defc906c48426b28882e780d6704805a9666ab1a91c0715b5cc27c378eb4de07c0d5d1578468b537596e12962eb20436d44cbcb3c80a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LearningDeviseForRails::Application.config.session_store :cookie_store, key: '_learning_devise_for_rails_session'
